60b6ee1e07d20c1aeb6e8dcb79ccbb7acd57e0c5
Arjun <arjun140702@gmail.com>
2022-03-03 14:08:16 +0530

    Set eslint rules, add starter code

--- bin/caa.js
+++ bin/caa.js
@@ -1,1 +1,30 @@
 #!/usr/bin/env node
+
+// import clear from 'clear'
+// import chalk from 'chalk'
+// import figlet from 'figlet'
+import { program } from 'commander'
+
+// clear()
+
+// console.log(
+//     chalk.yellow(
+//         figlet.textSync('create-any-app', { horizontalLayout: 'full' })
+//     )
+// )
+
+program
+    .name('create-any-app')
+    .description(
+        'CLI for rapid creation, configuration and development of JavaScript apps.'
+    )
+    .version('0.1.0', '-v, ,--vers, --version', 'print create-any-app version')
+
+program
+    .command('next <project-name>')
+    .description('Create a new Next.js project')
+    .action((name) => {
+        console.log(name)
+    })
+
+program.parse()

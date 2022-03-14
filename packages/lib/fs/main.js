3ce57665c3ad89aa4e8ce5a71aa6deaaa58a1240
Arjun <arjun140702@gmail.com>
2022-03-14 15:34:38 +0530

    Add commands for npm and git init, remove frameworks

--- packages/lib/fs/main.js
+++ packages/lib/fs/main.js
@@ -1,11 +1,23 @@
-import next from './types/next.js'
 import clear from 'clear'
 import shell from 'shelljs'
+import next from './types/next.js'
+import download from './download.js'
 
 export default function main(name, settings) {
     clear()
     shell.mkdir(name)
 
+    // Download template
+    download(
+        settings.type,
+        name,
+        settings.ts && settings.ts.use ? settings.ts.use : false
+    )
+
+    // Initialize npm
+    shell.exec('npm init -y')
+
+    // Perform removal as per settings
     switch (settings.type) {
         case 'next':
             next(name, settings)
@@ -13,4 +25,11 @@ export default function main(name, settings) {
         default:
             break
     }
+
+    // Setup Git repository
+    if (settings.git) {
+        shell.exec('git init')
+        shell.exec('git add .')
+        shell.exec('git commit -m "Initial commit"')
+    } else shell.rm('.gitignore')
 }
--- packages/lib/fs/types/next.js
+++ packages/lib/fs/types/next.js
@@ -1,10 +1,3 @@
-import download from '../download.js'
-
 export default function next(name, settings) {
-    download(
-        'next',
-        name,
-        settings.ts && settings.ts.use ? settings.ts.use : false
-    )
     console.log(settings)
 }
